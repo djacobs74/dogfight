656c12557f6316268aa32e461d47bbb679645656
David Jacobs <djacobs74@gmail.com>
2016-05-26 19:54:48 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogfight::Application.config.secret_key_base = 'e32833313dcae7fd9ec4e4180ed0a783183354392cd6514809886e188edfc2656ca1504ae26e091048a3274bc07b4b97f51a21b48f42731416f5fbff29916fae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dogfight::Application.config.session_store :cookie_store, key: '_dogfight_session'
